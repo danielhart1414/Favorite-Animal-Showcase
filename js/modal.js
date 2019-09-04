adac42bd3f4384226d4a1e4afa3d092b8d466feb
danielhart1414 <daniel_hart@live.com>
2019-09-04 11:55:38 -0400

    Modal opens to correct clicked image & closes w/ x

--- js/modal.js
+++ js/modal.js
@@ -2,6 +2,40 @@
 MODAL SCRIPT
 *******************************/
 
-function openModal() {
-    document.getElementById("animalModal").style.display = "block";
+function openAnimalModal() {
+  document.getElementById("animalModal").style.display = "block";
+}
+
+function closeAnimalModal() {
+  document.getElementById("animalModal").style.display = "none";
+}
+
+var slideIndex = 1;
+
+//Sets modal initially and provides thumbnail control
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+//Next/previous controls
+function advanceSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("animalSlide");
+  var imgThumbs = document.getElementsByClassName("imgThumb");
+  var captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < imgThumbs.length; i++) {
+    imgThumbs[i].classList.remove("active");
+  }
+  slides[slideIndex-1].style.display = "block";
+  imgThumbs[slideIndex-1].classList.add("active");
+  captionText.innerHTML = imgThumbs[slideIndex-1].alt;
 }
